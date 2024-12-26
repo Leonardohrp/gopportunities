79bc73283c8520ed74c14bc0089fd0deb1514732
Leonardo Pinheiro <leonardohrp@hotmail.com>
2024-12-26 19:02:08 -0300

    feat: Add request logic handlers for update, delete, create, show and list

--- handler/request.go
+++ handler/request.go
@@ -11,8 +11,25 @@ type CreateOpeningRequest struct {
 	Salary   int64  `json:"salary"`
 }
 
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
 func errParamIsRequired(name, typ string) error {
 	return fmt.Errorf("param %s (type %s) is required", name, typ)
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("as least one valid field must be provied")
 }
 
 func (r *CreateOpeningRequest) Validate() error {
